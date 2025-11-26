a15adeb3b45a3c19c5f87330645dd6b5871b1063
Vicente-FD <vi.fernandezd@duocuc.cl>
2025-11-26 14:44:41 -0300

    Actualiza los archivos de caché de Python y refactoriza el componente de análisis para eliminar código obsoleto y mejorar la gestión de filtros. Se simplifican las condiciones de selección de empresa y se optimizan las funciones de actualización de filtros y generación de gráficos, mejorando la claridad y eficiencia del código.

--- tacticsphere-frontend/src/environments/environment.ts
+++ tacticsphere-frontend/src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
   apiUrl: 'http://127.0.0.1:8000',
+  firebaseConfig : {
+    apiKey: "AIzaSyBt1BIg0s8AbCXGrZPkFVIUK0VBQqeIsvs",
+    authDomain: "tacticsphere-prod.firebaseapp.com",
+    projectId: "tacticsphere-prod",
+    storageBucket: "tacticsphere-prod.firebasestorage.app",
+    messagingSenderId: "100763235018",
+    appId: "1:100763235018:web:dba138b53e6f442af2ba33",
+    measurementId: "G-XXKRKQZGCG"
+  }  
 };
